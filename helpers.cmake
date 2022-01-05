7e9a39791eb241f701af7da152534bae4232c231
zhivko.petrov <zhivko.petrov@ocado.com>
2022-01-05 16:13:24 +0200

    feat: enable support for gcc and clang sanitizers

--- helpers.cmake
+++ helpers.cmake
@@ -16,28 +16,31 @@ endfunction()
 function(enable_target_warnings target)
     target_compile_options(
         ${target}
-        PRIVATE
-          -Wall
-          -Wextra
-          -Werror
-          -Wundef
-          -Wuninitialized
-          -Wreorder
-          -Wshadow
-          -Wpointer-arith
-          -Wcast-align
-          -Wcast-qual
-          -Wconversion
-          -Wunused-parameter
-          -Wlogical-op
-          -Wdouble-promotion
-          -Wuseless-cast
-          -Wnon-virtual-dtor
-          -Woverloaded-virtual
-          -Wduplicated-cond
-          -Wduplicated-branches
-          -Wnull-dereference
+            PRIVATE
+              -Wall
+              -Wextra
+              -Werror
+              -Wundef
+              -Wuninitialized
+              -Wshadow
+              -Wpointer-arith
+              -Wcast-align
+              -Wcast-qual
+              -Wunused-parameter
+              -Wdouble-promotion
+              -Wnull-dereference
     )
+    
+    if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
+        #supported only in GNU
+        target_compile_options(
+          ${target}
+              PRIVATE
+                -Wlogical-op
+                -Wduplicated-cond
+                -Wduplicated-branches
+        )
+    endif()
 endfunction()
 
 function(enable_target_include_what_you_use target)
@@ -57,17 +60,17 @@ function(enable_target_position_independent_code target)
     )
 endfunction()
 
-function(set_target_visibility)
+function(set_target_visibility target)
     if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
-    set(DEFAULT_BUILD_TYPE "Debug")
-      message(STATUS 
-        "Setting build type to '${DEFAULT_BUILD_TYPE}' as none was specified.")
-      set(CMAKE_BUILD_TYPE "${DEFAULT_BUILD_TYPE}" CACHE
-          STRING "Choose the type of build." FORCE)
+        set(DEFAULT_BUILD_TYPE "Debug")
+            message(STATUS 
+              "Setting build type to '${DEFAULT_BUILD_TYPE}' as none was specified.")
+            set(CMAKE_BUILD_TYPE "${DEFAULT_BUILD_TYPE}" CACHE
+                STRING "Choose the type of build." FORCE)
     endif()
     
-    if(CMAKE_BUILD_TYPE MATCHES Release OR 
-       CMAKE_BUILD_TYPE MATCHES MinSizeRel)
+    if(${CMAKE_BUILD_TYPE} MATCHES Release OR 
+       ${CMAKE_BUILD_TYPE} MATCHES MinSizeRel)
         # Default to hidden visibility for symbols
         set(CMAKE_CXX_VISIBILITY_PRESET hidden)
         set(CMAKE_VISIBILITY_INLINES_HIDDEN TRUE)
@@ -78,8 +81,8 @@ function(set_target_visibility)
                 CXX_VISIBILITY_PRESET hidden
                 VISIBILITY_INLINES_HIDDEN TRUE
         )
-    elseif(CMAKE_BUILD_TYPE MATCHES Debug OR 
-           CMAKE_BUILD_TYPE MATCHES RelWithDebInfo)  
+    elseif(${CMAKE_BUILD_TYPE} MATCHES Debug OR 
+           ${CMAKE_BUILD_TYPE} MATCHES RelWithDebInfo)  
         if(UNIX)
             set(R_DYNAMIC_FLAG "-rdynamic")
         elseif(APPLE)
@@ -88,8 +91,53 @@ function(set_target_visibility)
     endif()
     
     target_link_libraries(
-        ${PROJECT_NAME} 
+        ${target} 
             PRIVATE
             ${R_DYNAMIC_FLAG} # export of static symbols
     )   
+endfunction()
+
+#enable_target_c_sanitizer(${my_target} "address")
+# Available sanitizers
+#
+# GCC: address, thread, leak, undefined
+# CLANG: address, memory, thread, leak, undefined
+function(enable_target_sanitizer target sanitizer)
+    if(NOT CMAKE_BUILD_TYPE OR NOT ${CMAKE_BUILD_TYPE} MATCHES Debug)
+        message(
+            FATAL_ERROR
+            "Error: Sanitizers can be enabled only with 'Debug' build\n"
+            "Hint: Use 'cmake .. -DCMAKE_BUILD_TYPE=Debug'")
+        return()
+    endif()
+    
+    target_link_libraries(
+        ${target}
+            PRIVATE
+                -fsanitize=${sanitizer}
+    )
+    
+    if(${sanitizer} STREQUAL "address")
+        target_link_libraries(
+            ${target}
+                PRIVATE
+                    -fno-omit-frame-pointer
+        )
+    endif()
+    
+    if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
+        if (${sanitizer} STREQUAL "undefined")
+            target_link_libraries(
+                ${target}
+                    PRIVATE
+                        -lubsan
+            )
+        elseif (${sanitizer} STREQUAL "thread")
+            target_link_libraries(
+                ${target}
+                    PRIVATE
+                        -ltsan
+            )
+        endif()
+    endif()
 endfunction()
