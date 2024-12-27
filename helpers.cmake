b584eb1c4eff63aa8c864001e7cca9434b076d13
Zhivko Petrov <zhivko.petrov@lucidlink.com>
2024-12-27 16:38:30 +0200

    refactor: applidied unified naming convention

--- helpers.cmake
+++ helpers.cmake
@@ -1,79 +1,80 @@
 cmake_minimum_required(VERSION 3.10.2)
 
-list(APPEND 
-     CMAKE_MODULE_PATH 
-     ${CMAKE_CURRENT_LIST_DIR}/find_modules
+list(
+    APPEND 
+        CMAKE_MODULE_PATH 
+        ${CMAKE_CURRENT_LIST_DIR}/find_modules
 )
 
-function(set_target_cpp_standard target standard)
-set_target_properties(
-    ${target}
-    PROPERTIES
-        CXX_STANDARD ${standard}
-        CXX_STANDARD_REQUIRED YES
-        CXX_EXTENSIONS NO
-)
+function(set_target_cpp_standard Target Standard)
+    set_target_properties(
+        ${Target}
+            PROPERTIES
+                CXX_STANDARD ${Standard}
+                CXX_STANDARD_REQUIRED YES
+                CXX_EXTENSIONS NO
+    )
 endfunction()
 
-function(enable_target_warnings target)
+function(enable_target_warnings Target)
     if(CMAKE_CXX_COMPILER_ID MATCHES MSVC)
         target_compile_options(
-            ${target}
+            ${Target}
                 PRIVATE
-                  /W4
-                  /WX
+                    /W4
+                    /WX
         )
         return()
     endif()
     
     target_compile_options(
-        ${target}
+        ${Target}
             PRIVATE
-              -Wall
-              -Wextra
-              -Werror
-              -Wundef
-              -Wuninitialized
-              -Wshadow
-              -Wpointer-arith
-              -Wcast-align
-              -Wcast-qual
-              -Wunused-parameter
-              -Wdouble-promotion
-              -Wnull-dereference
+                -Wall
+                -Wextra
+                -Werror
+                -Wundef
+                -Wuninitialized
+                -Wshadow
+                -Wpointer-arith
+                -Wcast-align
+                -Wcast-qual
+                -Wunused-parameter
+                -Wdouble-promotion
+                -Wnull-dereference
     )
     
     if(CMAKE_CXX_COMPILER_ID MATCHES GNU AND NOT ${USE_IWYU})
         #supported only in GNU
         #however include-what-you-use is not happy with those options
         target_compile_options(
-          ${target}
-              PRIVATE
-                -Wlogical-op
-                -Wduplicated-cond
-                -Wduplicated-branches
+            ${Target}
+                PRIVATE
+                    -Wlogical-op
+                    -Wduplicated-cond
+                    -Wduplicated-branches
         )
     endif()
 endfunction()
 
-function(enable_target_include_what_you_use target)
+function(enable_target_include_what_you_use Target)
     find_package(IWYU REQUIRED)
     set_target_properties(
-    ${target}
-        PROPERTIES 
-        CXX_INCLUDE_WHAT_YOU_USE ${IWYU_BINARY_PATH}
-    )
-endfunction()
-
-function(enable_target_position_independent_code target)
-    set_target_properties(
-        ${target}
+        ${Target}
             PROPERTIES 
-            POSITION_INDEPENDENT_CODE ON
+                CXX_INCLUDE_WHAT_YOU_USE ${IWYU_BINARY_PATH}
     )
 endfunction()
 
-function(set_target_visibility target)
+function(enable_target_position_independent_code Target)
+    set_target_properties(
+        ${Target}
+            PROPERTIES 
+                POSITION_INDEPENDENT_CODE ON
+    )
+endfunction()
+
+function(set_target_visibility Target)
     if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
         set(DEFAULT_BUILD_TYPE "Debug")
             message(STATUS 
@@ -85,23 +86,23 @@ function(set_target_visibility target)
     if((${CMAKE_BUILD_TYPE} MATCHES Release) OR 
        (${CMAKE_BUILD_TYPE} MATCHES MinSizeRel))        
         set_target_properties(
-            ${target}
-             PROPERTIES 
-                CXX_VISIBILITY_PRESET hidden
-                VISIBILITY_INLINES_HIDDEN TRUE
+            ${Target}
+                PROPERTIES 
+                    CXX_VISIBILITY_PRESET hidden
+                    VISIBILITY_INLINES_HIDDEN TRUE
         )
     elseif((${CMAKE_BUILD_TYPE} MATCHES Debug) OR 
            (${CMAKE_BUILD_TYPE} MATCHES RelWithDebInfo))  
         if(UNIX)
-            set(R_DYNAMIC_FLAG "-rdynamic")
+            set(ExportDynamicFlag "-rdynamic")
         elseif(APPLE)
-            set(R_DYNAMIC_FLAG "-Wl,-export_dynamic")
+            set(ExportDynamicFlag "-Wl,-export_dynamic")
         endif()
         
         target_link_libraries(
-            ${target} 
+            ${Target} 
                 PRIVATE
-                    ${R_DYNAMIC_FLAG} # export of static symbols
+                    ${ExportDynamicFlag} # export of static symbols
         ) 
     endif()
     
@@ -113,7 +114,7 @@ endfunction()
 #
 # GCC: address, thread, leak, undefined
 # CLANG: address, memory, thread, leak, undefined
-function(enable_target_sanitizer target sanitizer)
+function(enable_target_sanitizer Target Sanitizer)
     if(NOT CMAKE_BUILD_TYPE OR NOT ${CMAKE_BUILD_TYPE} MATCHES Debug)
         message(
             FATAL_ERROR
@@ -123,29 +124,29 @@ function(enable_target_sanitizer target sanitizer)
     endif()
     
     target_link_libraries(
-        ${target}
+        ${Target}
             PRIVATE
-                -fsanitize=${sanitizer}
+                -fsanitize=${Sanitizer}
     )
     
-    if(${sanitizer} STREQUAL "address")
+    if(${Sanitizer} STREQUAL "address")
         target_link_libraries(
-            ${target}
+            ${Target}
                 PRIVATE
                     -fno-omit-frame-pointer
         )
     endif()
     
     if(CMAKE_CXX_COMPILER_ID MATCHES GNU)
-        if (${sanitizer} STREQUAL "undefined")
+        if (${Sanitizer} STREQUAL "undefined")
             target_link_libraries(
-                ${target}
+                ${Target}
                     PRIVATE
                         -lubsan
             )
-        elseif (${sanitizer} STREQUAL "thread")
+        elseif (${Sanitizer} STREQUAL "thread")
             target_link_libraries(
-                ${target}
+                ${Target}
                     PRIVATE
                         -ltsan
             )
@@ -173,32 +174,32 @@ function(set_default_project_output_directories)
 
     # Explicitly set output directories for all build types (Debug, Release, etc.)
     # Cheers for MSVC
-    foreach(OUTPUTCONFIG ${CMAKE_CONFIGURATION_TYPES})
-        string(TOUPPER ${OUTPUTCONFIG} OUTPUTCONFIG)
+    foreach(OutputConfig ${CMAKE_CONFIGURATION_TYPES})
+        string(TOUPPER ${OutputConfig} OutputConfig)
 
         set(
-            CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OUTPUTCONFIG} 
+            CMAKE_ARCHIVE_OUTPUT_DIRECTORY_${OutputConfig} 
             ${CMAKE_BINARY_DIR}/lib PARENT_SCOPE
         )
 
         set(
-            CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OUTPUTCONFIG} 
+            CMAKE_LIBRARY_OUTPUT_DIRECTORY_${OutputConfig} 
             ${CMAKE_BINARY_DIR}/bin PARENT_SCOPE
         )
 
         set(
-            CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OUTPUTCONFIG} 
+            CMAKE_RUNTIME_OUTPUT_DIRECTORY_${OutputConfig} 
             ${CMAKE_BINARY_DIR}/bin PARENT_SCOPE
         )
     endforeach()
 endfunction()
 
-function(install_and_export_target target include_folder_name)
+function(install_and_export_target Target IncludeFolderName)
     install(
         TARGETS 
-            ${target} 
+            ${Target} 
         EXPORT 
-            ${target}Targets
+            ${Target}Targets
         LIBRARY 
             DESTINATION ${CMAKE_INSTALL_LIBDIR}
         RUNTIME 
@@ -206,97 +207,101 @@ function(install_and_export_target target include_folder_name)
     )
     
     install(
-        DIRECTORY ${include_folder_name}/
-        DESTINATION ${include_folder_name}
+        DIRECTORY ${IncludeFolderName}/
+        DESTINATION ${IncludeFolderName}
     )
     
     install(
-        EXPORT ${target}Targets
-        DESTINATION lib/cmake/${target}
-        FILE ${target}Targets.cmake
-        NAMESPACE ${target}::
+        EXPORT ${Target}Targets
+        DESTINATION lib/cmake/${Target}
+        FILE ${Target}Targets.cmake
+        NAMESPACE ${Target}::
     )
     
     include(CMakePackageConfigHelpers)
     write_basic_package_version_file(
-        ${CMAKE_CURRENT_BINARY_DIR}/${target}ConfigVersion.cmake 
+        ${CMAKE_CURRENT_BINARY_DIR}/${Target}ConfigVersion.cmake 
         VERSION 1.0.0
         COMPATIBILITY SameMajorVersion
     )
     
     install(
-        FILES ${target}Config.cmake
-        DESTINATION lib/cmake/${target}
+        FILES ${Target}Config.cmake
+        DESTINATION lib/cmake/${Target}
     )
 endfunction()
 
 # Requires package.xml file to be present in the current directory
-function(enable_ros_tooling_for_target target package_xml)
+function(enable_ros_tooling_for_target Target PackageXml)
     find_package(ament_cmake REQUIRED)
+
+    set(TargetPath share/${Target})
 
     # Install package.xml file so this package can be processed by ROS toolings
     # Installing this in non-ROS environments won't have any effect, but it won't harm, either.
     install(
-        FILES ${package_xml} 
-        DESTINATION share/${target}
+        FILES ${PackageXml} 
+        DESTINATION ${TargetPath}
     )
     
-    # Install config directory (if any)
-    get_filename_component(_FULL_LAUNCH_PATH launch REALPATH)
-	if ((EXISTS ${_FULL_LAUNCH_PATH}) AND (IS_DIRECTORY ${_FULL_LAUNCH_PATH}))
+    # Install launch directory (if any)
+    get_filename_component(FullLaunchPath launch REALPATH)
+	if ((EXISTS ${FullLaunchPath}) AND (IS_DIRECTORY ${FullLaunchPath}))
 		install(
 			DIRECTORY launch 
-			DESTINATION share/${target}/
+			DESTINATION ${TargetPath}/
 		)
 	endif()
 	
-	# Install launch directory (if any)
-	get_filename_component(_FULL_CONFIG_PATH launch REALPATH)
-	if ((EXISTS ${_FULL_CONFIG_PATH}) AND (IS_DIRECTORY ${_FULL_CONFIG_PATH}))
+	# Install config directory (if any)
+	get_filename_component(FullConfigPath launch REALPATH)
+	if ((EXISTS ${FullConfigPath}) AND (IS_DIRECTORY ${FullConfigPath}))
 		install(
 			DIRECTORY config 
-			DESTINATION share/${target}/
+			DESTINATION ${TargetPath}/
 		)
 	endif()
     
     # ros2 run requires both libraries and binaries to be placed in 'lib'
-    install(TARGETS
-        ${target}
-        DESTINATION lib/${target}
+    install(
+        TARGETS ${Target}
+        DESTINATION lib/${Target}
     )
     
     # Allows Colcon to find non-Ament packages when using workspace underlays
+    set(RsrcIndexPkgPath share/ament_index/resource_index/packages)
+    set(RsrcIndexPkgPathTarget ${CMAKE_CURRENT_BINARY_DIR}/${RsrcIndexPkgPath}/${Target})
     file(
         WRITE 
-        ${CMAKE_CURRENT_BINARY_DIR}/share/ament_index/resource_index/packages/${target} 
-        ""
+            ${RsrcIndexPkgPathTarget} 
+            ""
     )
     install(
-        FILES 
-        ${CMAKE_CURRENT_BINARY_DIR}/share/ament_index/resource_index/packages/${target}
-        DESTINATION share/ament_index/resource_index/packages
+        FILES ${RsrcIndexPkgPathTarget}
+        DESTINATION ${RsrcIndexPkgPath}
     )
     
+    set(AmentHookPath ${TargetPath}/hook)
+    set(AmentPrefixPath ${CMAKE_CURRENT_BINARY_DIR}/${AmentHookPath}/ament_prefix_path.dsv)
     file(
         WRITE 
-        ${CMAKE_CURRENT_BINARY_DIR}/share/${target}/hook/ament_prefix_path.dsv
-        "prepend-non-duplicate;AMENT_PREFIX_PATH;"
+            ${AmentPrefixPath}
+            "prepend-non-duplicate;AMENT_PREFIX_PATH;"
     )
     install(
-        FILES ${CMAKE_CURRENT_BINARY_DIR}/share/${target}/hook/ament_prefix_path.dsv
-        DESTINATION
-        share/${target}/hook
+        FILES ${AmentPrefixPath}
+        DESTINATION ${AmentHookPath}
     )
     
+    set(RosPackagePath ${CMAKE_CURRENT_BINARY_DIR}/${AmentHookPath}/ros_package_path.dsv)
     file(
         WRITE
-        ${CMAKE_CURRENT_BINARY_DIR}/share/${target}/hook/ros_package_path.dsv
-        "prepend-non-duplicate;ROS_PACKAGE_PATH;"
+            ${RosPackagePath}
+            "prepend-non-duplicate;ROS_PACKAGE_PATH;"
     )
     install(
-        FILES
-        ${CMAKE_CURRENT_BINARY_DIR}/share/${target}/hook/ros_package_path.dsv
-        DESTINATION share/${target}/hook
+        FILES ${RosPackagePath}
+        DESTINATION ${AmentHookPath}
     )
 
     ament_package()
